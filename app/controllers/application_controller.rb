1434711e3671cdec1aca3200fa50a743ad0e65e2
Eric Mayfield <eric@visiblycreative.com>
2019-01-12 13:10:25 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,26 +16,37 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-
+    @user = User.create(params)
+    @user.save
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
   get '/sessions/login' do
+    if session[:message]
+      @message = session[:message]
+    end
     erb :'sessions/login'
   end
 
   post '/sessions' do
-
-    redirect '/users/home'
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user != nil
+      session[:id] = @user.id
+      redirect '/users/home'
+    else
+      session[:message] = "Incorrect User Name or Password"
+      redirect '/sessions/login'
+    end
   end
 
   get '/sessions/logout' do 
-
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
-
+    @user = User.find_by_id(session[:id])
     erb :'/users/home'
   end
 
